75dc74ead4795141074ab673b1f5588b7983e1f0
gagicanja <81252270+gagicanja@users.noreply.github.com>
2021-03-26 21:12:29 +0100

    Pocetna verzija

---
+++ Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace Project
+{
+    partial class DePicto
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DePicto));
+            this.pnlCrtanje = new System.Windows.Forms.Panel();
+            this.picOlovka = new System.Windows.Forms.PictureBox();
+            this.picGumica = new System.Windows.Forms.PictureBox();
+            this.picText = new System.Windows.Forms.PictureBox();
+            this.picCrna = new System.Windows.Forms.PictureBox();
+            this.picSiva = new System.Windows.Forms.PictureBox();
+            this.picCrvena = new System.Windows.Forms.PictureBox();
+            this.picNarandzasta = new System.Windows.Forms.PictureBox();
+            this.picZuta = new System.Windows.Forms.PictureBox();
+            this.picZelena = new System.Windows.Forms.PictureBox();
+            this.picPlava = new System.Windows.Forms.PictureBox();
+            this.picLjubicasta = new System.Windows.Forms.PictureBox();
+            this.picRoza = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picOlovka)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGumica)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picText)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCrna)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSiva)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCrvena)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picNarandzasta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picZuta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picZelena)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlava)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLjubicasta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picRoza)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlCrtanje
+            // 
+            this.pnlCrtanje.Location = new System.Drawing.Point(0, 35);
+            this.pnlCrtanje.Name = "pnlCrtanje";
+            this.pnlCrtanje.Size = new System.Drawing.Size(1017, 412);
+            this.pnlCrtanje.TabIndex = 2;
+            this.pnlCrtanje.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlCrtanje_Paint);
+            this.pnlCrtanje.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlCrtanje_MouseDown);
+            this.pnlCrtanje.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnlCrtanje_MouseMove);
+            this.pnlCrtanje.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pnlCrtanje_MouseUp);
+            // 
+            // picOlovka
+            // 
+            this.picOlovka.BackColor = System.Drawing.Color.Transparent;
+            this.picOlovka.BackgroundImage = global::Project.Properties.Resources.pencil;
+            this.picOlovka.ErrorImage = global::Project.Properties.Resources.pencil;
+            this.picOlovka.Image = ((System.Drawing.Image)(resources.GetObject("picOlovka.Image")));
+            this.picOlovka.InitialImage = global::Project.Properties.Resources.pencil;
+            this.picOlovka.Location = new System.Drawing.Point(0, -1);
+            this.picOlovka.Name = "picOlovka";
+            this.picOlovka.Size = new System.Drawing.Size(31, 30);
+            this.picOlovka.TabIndex = 0;
+            this.picOlovka.TabStop = false;
+            this.picOlovka.Click += new System.EventHandler(this.picOlovka_Click);
+            // 
+            // picGumica
+            // 
+            this.picGumica.BackColor = System.Drawing.SystemColors.Control;
+            this.picGumica.ErrorImage = ((System.Drawing.Image)(resources.GetObject("picGumica.ErrorImage")));
+            this.picGumica.Image = global::Project.Properties.Resources.olovka1;
+            this.picGumica.Location = new System.Drawing.Point(37, -1);
+            this.picGumica.Name = "picGumica";
+            this.picGumica.Size = new System.Drawing.Size(31, 30);
+            this.picGumica.TabIndex = 3;
+            this.picGumica.TabStop = false;
+            this.picGumica.Click += new System.EventHandler(this.picGumica_Click);
+            // 
+            // picText
+            // 
+            this.picText.BackColor = System.Drawing.Color.DarkGray;
+            this.picText.Location = new System.Drawing.Point(74, -1);
+            this.picText.Name = "picText";
+            this.picText.Size = new System.Drawing.Size(31, 30);
+            this.picText.TabIndex = 4;
+            this.picText.TabStop = false;
+            // 
+            // picCrna
+            // 
+            this.picCrna.BackColor = System.Drawing.Color.Black;
+            this.picCrna.Location = new System.Drawing.Point(111, -1);
+            this.picCrna.Name = "picCrna";
+            this.picCrna.Size = new System.Drawing.Size(31, 30);
+            this.picCrna.TabIndex = 5;
+            this.picCrna.TabStop = false;
+            // 
+            // picSiva
+            // 
+            this.picSiva.BackColor = System.Drawing.Color.Gray;
+            this.picSiva.Location = new System.Drawing.Point(148, -1);
+            this.picSiva.Name = "picSiva";
+            this.picSiva.Size = new System.Drawing.Size(31, 30);
+            this.picSiva.TabIndex = 6;
+            this.picSiva.TabStop = false;
+            // 
+            // picCrvena
+            // 
+            this.picCrvena.BackColor = System.Drawing.Color.Red;
+            this.picCrvena.Location = new System.Drawing.Point(185, -1);
+            this.picCrvena.Name = "picCrvena";
+            this.picCrvena.Size = new System.Drawing.Size(31, 30);
+            this.picCrvena.TabIndex = 7;
+            this.picCrvena.TabStop = false;
+            // 
+            // picNarandzasta
+            // 
+            this.picNarandzasta.BackColor = System.Drawing.Color.Orange;
+            this.picNarandzasta.Location = new System.Drawing.Point(222, -1);
+            this.picNarandzasta.Name = "picNarandzasta";
+            this.picNarandzasta.Size = new System.Drawing.Size(31, 30);
+            this.picNarandzasta.TabIndex = 8;
+            this.picNarandzasta.TabStop = false;
+            // 
+            // picZuta
+            // 
+            this.picZuta.BackColor = System.Drawing.Color.Yellow;
+            this.picZuta.Location = new System.Drawing.Point(259, -1);
+            this.picZuta.Name = "picZuta";
+            this.picZuta.Size = new System.Drawing.Size(31, 30);
+            this.picZuta.TabIndex = 9;
+            this.picZuta.TabStop = false;
+            // 
+            // picZelena
+            // 
+            this.picZelena.BackColor = System.Drawing.Color.LimeGreen;
+            this.picZelena.Location = new System.Drawing.Point(296, -1);
+            this.picZelena.Name = "picZelena";
+            this.picZelena.Size = new System.Drawing.Size(31, 30);
+            this.picZelena.TabIndex = 10;
+            this.picZelena.TabStop = false;
+            // 
+            // picPlava
+            // 
+            this.picPlava.BackColor = System.Drawing.Color.Blue;
+            this.picPlava.Location = new System.Drawing.Point(333, -1);
+            this.picPlava.Name = "picPlava";
+            this.picPlava.Size = new System.Drawing.Size(31, 30);
+            this.picPlava.TabIndex = 11;
+            this.picPlava.TabStop = false;
+            // 
+            // picLjubicasta
+            // 
+            this.picLjubicasta.BackColor = System.Drawing.Color.Purple;
+            this.picLjubicasta.Location = new System.Drawing.Point(370, -1);
+            this.picLjubicasta.Name = "picLjubicasta";
+            this.picLjubicasta.Size = new System.Drawing.Size(31, 30);
+            this.picLjubicasta.TabIndex = 12;
+            this.picLjubicasta.TabStop = false;
+            // 
+            // picRoza
+            // 
+            this.picRoza.BackColor = System.Drawing.Color.Pink;
+            this.picRoza.Location = new System.Drawing.Point(407, -1);
+            this.picRoza.Name = "picRoza";
+            this.picRoza.Size = new System.Drawing.Size(31, 30);
+            this.picRoza.TabIndex = 13;
+            this.picRoza.TabStop = false;
+            // 
+            // DePicto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1017, 440);
+            this.Controls.Add(this.picRoza);
+            this.Controls.Add(this.picLjubicasta);
+            this.Controls.Add(this.picPlava);
+            this.Controls.Add(this.picZelena);
+            this.Controls.Add(this.picZuta);
+            this.Controls.Add(this.picNarandzasta);
+            this.Controls.Add(this.picCrvena);
+            this.Controls.Add(this.picSiva);
+            this.Controls.Add(this.picCrna);
+            this.Controls.Add(this.picText);
+            this.Controls.Add(this.picGumica);
+            this.Controls.Add(this.picOlovka);
+            this.Controls.Add(this.pnlCrtanje);
+            this.Name = "DePicto";
+            this.Text = "DePicto";
+            this.Load += new System.EventHandler(this.DePicto_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picOlovka)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGumica)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picText)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCrna)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSiva)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCrvena)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picNarandzasta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picZuta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picZelena)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlava)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLjubicasta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picRoza)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel pnlCrtanje;
+        private System.Windows.Forms.PictureBox picOlovka;
+        private System.Windows.Forms.PictureBox picGumica;
+        private System.Windows.Forms.PictureBox picText;
+        private System.Windows.Forms.PictureBox picCrna;
+        private System.Windows.Forms.PictureBox picSiva;
+        private System.Windows.Forms.PictureBox picCrvena;
+        private System.Windows.Forms.PictureBox picNarandzasta;
+        private System.Windows.Forms.PictureBox picZuta;
+        private System.Windows.Forms.PictureBox picZelena;
+        private System.Windows.Forms.PictureBox picPlava;
+        private System.Windows.Forms.PictureBox picLjubicasta;
+        private System.Windows.Forms.PictureBox picRoza;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class DePicto : Form
+    {
+        Graphics g;
+        Pen p;
+        bool pomeranje = false;
+        int x, y;
+        Color trenBoja;
+        public DePicto()
+        {
+            InitializeComponent();
+            g = pnlCrtanje.CreateGraphics();
+            p = new Pen(Color.Black, 3);
+            x = -1; y = -1;
+            trenBoja = Color.Black;
+        }
+
+        private void DePicto_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlCrtanje_MouseDown(object sender, MouseEventArgs e)
+        {
+            pomeranje = true;
+            x = e.X;
+            y = e.Y;
+
+        }
+
+        private void pnlCrtanje_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(pomeranje && x!=-1 && x != -1)
+            {
+                g.DrawLine(p, new Point(x, y), e.Location);
+                x = e.X;
+                y = e.Y;
+            }
+        }
+
+        private void picGumica_Click(object sender, EventArgs e)
+        {
+            p = new Pen(BackColor, 20);
+        }
+
+        private void picOlovka_Click(object sender, EventArgs e)
+        {
+            p = new Pen(trenBoja);
+        }
+
+
+        private void pnlCrtanje_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+
+        private void pnlCrtanje_MouseUp(object sender, MouseEventArgs e)
+        {
+            pomeranje = false;
+            x = -1;
+            y = -1;
+        }
+    }
+}
